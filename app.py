2a294e349c740d35471d66a132f6edbd68fbc6ae
Geo Ema <emanueltula89@gmail.com>
2025-06-28 17:52:44 -0300

    removed unused matplotlib import to fix deployment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt  <-- Esta línea borrada o comentada
 import altair as alt
 
 # 1. Título de la aplicación
